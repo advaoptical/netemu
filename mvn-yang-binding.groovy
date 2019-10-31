15d4e3cacfd6394e889588e4c3b9a9b714e85401
Stefan Zimmermann <szimmermann@advaoptical.com>
2019-10-31 12:44:27 +0100

    Add mvn wrapper script for ODL yang-binding

---
+++ mvn-yang-binding.groovy
@@ -0,0 +1,24 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+def YANG_BINDING_HOME = Paths.get(
+    "opendaylight-mdsal", "binding", "yang-binding")
+
+def JAVA_FILES = Files.walk(YANG_BINDING_HOME).filter { path ->
+    path.fileName.toString().endsWith ".java"
+}.toList()
+
+for (file in JAVA_FILES) {
+    file.text = file.text.replace "\r\n", "\n"
+}
+
+new ProcessBuilder("mvn", *args)
+    .directory(YANG_BINDING_HOME.toFile())
+    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+    .redirectError(ProcessBuilder.Redirect.INHERIT)
+    .start()
+    .waitFor()
+
+for (file in JAVA_FILES) {
+    file.text = file.text.replace "\n", "\r\n"
+}
