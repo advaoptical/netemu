caae57bff04217111d173e49c802967434e71939
Stefan Zimmermann <szimmermann@advaoptical.com>
2020-01-08 16:00:04 +0100

    NetEmu: Add .loadConfigurationFromXml() methods
    Supporting File and Reader input

--- src/main/java/com/adva/netemu/NetEmu.java
+++ src/main/java/com/adva/netemu/NetEmu.java
@@ -2,9 +2,23 @@ package com.adva.netemu;
 
 // import org.graalvm.compiler.hotspot.stubs.StubUtil;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import javax.annotation.Nonnull;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import com.google.common.collect.ImmutableSet;
 
-import org.eclipse.jdt.annotation.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.opendaylight.netconf.test.tool.NetconfDeviceSimulator;
 import org.opendaylight.netconf.test.tool.config.ConfigurationBuilder;
@@ -12,14 +26,20 @@ import org.opendaylight.netconf.test.tool.config.ConfigurationBuilder;
 
 public final class NetEmu extends NetconfDeviceSimulator {
 
-    @NonNull
+    private static final Logger LOG = LoggerFactory.getLogger(NetEmu.class);
+
+    private static final XMLInputFactory XML_INPUT_FACTORY =
+            XMLInputFactory.newInstance();
+
+    @Nonnull
     private final YangPool _pool;
 
+    @Nonnull
     public YangPool getYangPool() {
         return this._pool;
     }
 
-    public NetEmu(@NonNull final YangPool pool) {
+    public NetEmu(@Nonnull final YangPool pool) {
         super(new ConfigurationBuilder()
                 .setCapabilities(ImmutableSet.of(
                         "urn:ietf:params:netconf:base:1.0",
@@ -30,5 +50,54 @@ public final class NetEmu extends NetconfDeviceSimulator {
                 .build());
 
         this._pool = pool;
+    }
+
+    public void loadConfigurationFromXml(@Nonnull final Reader reader) {
+        final XMLStreamReader xmlReader;
+        try {
+            xmlReader = XML_INPUT_FACTORY.createXMLStreamReader(reader);
+
+        } catch (XMLStreamException e) {
+            LOG.error("Cannot use reader for loading XML Configuration: "
+                    + reader);
+
+            e.printStackTrace();
+            LOG.error("Failed reading XML Configuration from: " + reader);
+            return;
+        }
+
+        this._pool.writeConfigurationDataFrom(xmlReader);
+    }
+
+    public void loadConfigurationFromXml(
+            @Nonnull final File file, @Nonnull final Charset encoding) {
+
+        final XMLStreamReader xmlReader;
+        try {
+            xmlReader = XML_INPUT_FACTORY.createXMLStreamReader(
+                    new FileReader(file, encoding));
+
+        } catch (final
+                IOException |
+                XMLStreamException e) {
+
+            LOG.error("Cannot open file for loading XML Configuration: "
+                    + file);
+
+            e.printStackTrace();
+            LOG.error("Failed reading XML Configuration from: " + file);
+            return;
+        }
+
+        this._pool.writeConfigurationDataFrom(xmlReader);
+    }
+
+    public void loadConfigurationFromXml(@Nonnull final File file) {
+        this.loadConfigurationFromXml(file, UTF_8);
+    }
+
+    public void loadConfigurationFromXml() {
+        this.loadConfigurationFromXml(
+                new File("configuration.xml").getAbsoluteFile());
     }
 }
