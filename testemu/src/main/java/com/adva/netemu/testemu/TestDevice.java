1800559691fd530a4594ad7cdc853b4410e435e8
Stefan Zimmermann <szimmermann@advaoptical.com>
2022-12-09 13:10:28 +0100

    testemu: Adopt latest NETEMU features

--- testemu/src/main/java/com/adva/netemu/testemu/TestDevice.java
+++ testemu/src/main/java/com/adva/netemu/testemu/TestDevice.java
@@ -2,22 +2,29 @@ package com.adva.netemu.testemu;
 
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Set;
 
+import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 
 import one.util.streamex.StreamEx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.adva.netemu.Owned;
 import com.adva.netemu.YangBindable;
 import com.adva.netemu.YangBinding;
 import com.adva.netemu.YangBound;
+import com.adva.netemu.YangListBound;
 
 
 @YangBound(context = NetEmuDefined.class, namespace = "urn:ietf:params:xml:ns:yang:ietf-interfaces", value = "interfaces")
 public class TestDevice implements YangBindable {
+
+    @Nonnull
+    private static final Logger LOG = LoggerFactory.getLogger(TestDevice.class);
 
     @Nonnull
     private final TestDevice_YangBinding yangBinding = new TestDevice_YangBinding();
@@ -28,40 +35,73 @@ public class TestDevice implements YangBindable {
     }
 
     @Nonnull
-    private final AtomicReference<List<TestInterface>> interfaces = new AtomicReference<>(List.of());
+    private final List<String> interfaceIndexRegistry = new ArrayList<>();
+
+    @Nonnegative
+    private int getInterfaceIndexForName(@Nonnull final String interfaceName) {
+        int interfaceIndex;
+        synchronized (this.interfaceIndexRegistry) {
+            interfaceIndex = this.interfaceIndexRegistry.indexOf(interfaceName);
+
+            if (interfaceIndex == -1) {
+                interfaceIndex = this.interfaceIndexRegistry.size();
+                this.interfaceIndexRegistry.add(interfaceName);
+            }
+        }
+
+        return interfaceIndex;
+    }
 
     @Nonnull
-    public List<TestInterface> interfaces() {
-        return List.copyOf(this.interfaces.get());
+    private final YangListBound.Collection<TestInterface> interfaces;
+
+    @Nonnull
+    public Set<TestInterface> interfaces() {
+        return Set.copyOf(this.interfaces);
     }
 
     public TestDevice() {
-        try {
-            this.interfaces.set(StreamEx.of(NetworkInterface.getNetworkInterfaces()).nonNull().map(TestInterface::from)
-                    .toImmutableList());
-
-        } catch (final SocketException e) {
-            throw new RuntimeException(e);
-        }
+        this.interfaceIndexRegistry.add(null); // preserve 0-index, because if-index must be >= 1
+        this.interfaces = new YangListBound.Collection<>(this.yangBinding);
 
         this.yangBinding.appliesConfigurationDataUsing(data -> {
-            this.interfaces.set(Owned.by(this, data.streamInterface().map(TestInterface_Yang.Data::from)
-                    .map(TestInterface::fromConfigurationData)
-                    .toImmutableList()));
+            @Nonnull final var newInterfaces = this.interfaces.mergeAll(
+                    data.streamInterface().map(interfaceData -> TestInterface
+                            .withIndex(this.getInterfaceIndexForName(interfaceData.requireName()))
+                            .fromConfigurationData(TestInterface_Yang.Data.from(interfaceData))),
+
+                    (existingInterface, iface) -> {});
+
+            LOG.info("New interfaces: {}", newInterfaces);
 
         }).appliesOperationalDataUsing(data -> {
             synchronized (this.interfaces) {
                 for (@Nonnull final var interfaceData : data.streamInterface().map(TestInterface_Yang.Data::from)) {
-                    TestInterface_Yang.bindingStreamOf(this.interfaces.get())
-                            .findFirst(binding -> interfaceData.getName().map(binding.getKey().getName()::equals)
-                                    .orElseThrow(() -> new IllegalArgumentException("No 'name' leaf value present in YANG Data")))
-
-                            .ifPresent(binding -> binding.applyOperationalData(interfaceData));
+                    TestInterface_Yang.bindingStreamOf(this.interfaces)
+                            .findFirst(binding -> interfaceData.requireName().equals(binding.getKey().getName()))
+                            .ifPresent(binding -> {
+                                binding.applyOperationalData(interfaceData);
+                            });
                 }
             }
 
-        }).providesOperationalDataUsing(builder -> builder
-                .setInterface(TestInterface_Yang.listOperationalDataFrom(this.interfaces.get())));
+        }).providesConfigurationDataUsing(builder -> {
+            @Nonnull final List<TestInterface> newInterfaces;
+            try {
+                newInterfaces = this.interfaces.mergeAll(
+                        StreamEx.of(NetworkInterface.getNetworkInterfaces()).nonNull().map(adapter ->
+                                TestInterface.withIndex(this.getInterfaceIndexForName(adapter.getName())).fromAdapter(adapter)),
+
+                        (existingInterface, iface) -> {});
+
+            } catch (final SocketException e) {
+                throw new RuntimeException(e);
+            }
+
+            LOG.info("New interfaces: {}", newInterfaces);
+            return builder
+                    .setInterface(TestInterface_Yang.listConfigurationDataFrom(newInterfaces));
+        });
     }
 
     @Nonnull
--- testemu/src/main/java/com/adva/netemu/testemu/TestInterface.java
+++ testemu/src/main/java/com/adva/netemu/testemu/TestInterface.java
@@ -2,9 +2,12 @@ package com.adva.netemu.testemu;
 
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.xml.bind.DatatypeConverter;
 
@@ -13,12 +16,14 @@ import com.adva.netemu.YangListBinding;
 import com.adva.netemu.YangListBound;
 
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev170119.EthernetCsmacd;
+
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev180220.InterfaceType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.DateAndTime;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
 
 
-@YangListBound(
-        context = NetEmuDefined.class, namespace = "urn:ietf:params:xml:ns:yang:ietf-interfaces", value = "interfaces/interface")
+@YangListBound(context = NetEmuDefined.class, namespace = "urn:ietf:params:xml:ns:yang:ietf-interfaces",
+        value = "interfaces/interface")
 
 public class TestInterface implements YangListBindable {
 
@@ -52,7 +57,7 @@ public class TestInterface implements YangListBindable {
         return !this.enabled.get();
     }
 
-    private TestInterface(@Nonnull final NetworkInterface adapter) {
+    private TestInterface(@Nonnegative final int index, @Nonnull final NetworkInterface adapter) {
         this.adapter = adapter;
 
         @Nonnull final Optional<String> macAddress;
@@ -64,47 +69,72 @@ public class TestInterface implements YangListBindable {
             throw new RuntimeException(e);
         }
 
-        this.yangBinding = TestInterface_YangBinding.withKey(TestInterface_Yang.ListKey.from(adapter.getName()))
-
+        this.yangBinding = TestInterface_YangBinding.withKey(TestInterface_Yang.ListKey.from(adapter.getName()));
+        this.yangBinding
                 .appliesConfigurationDataUsing(data -> {
                     data.getEnabled().ifPresent(this.enabled::set);
-                })
 
-                .appliesOperationalDataUsing(data -> {
+                }).appliesOperationalDataUsing(data -> {
                     data.getEnabled().ifPresent(this.enabled::set);
-                })
 
-                .providesOperationalDataUsing(builder -> builder
+                }).providesConfigurationDataUsing(builder -> builder
                         .setType(IETF_INTERFACE_TYPE)
                         .setName(adapter.getName())
+                        .setDescription(adapter.getDisplayName())
+
                         .setEnabled(this.enabled.get())
 
-                        .setDescription(adapter.getDisplayName())
-                        .setPhysAddress(macAddress.map(PhysAddress::new)));
+                ).providesOperationalDataUsing(builder -> builder
+                        .setIfIndex(index)
+                        .setName(adapter.getName())
+                        .setPhysAddress(macAddress.map(PhysAddress::new))
+
+                        .setAdminStatus_Up_if(this::isEnabled)
+                        .setAdminStatus_Down_if(this::isDisabled)
+
+                        .setOperStatus_Up_if(this::isEnabled)
+                        .setOperStatus_Down_if(this::isDisabled)
+
+                        .setStatistics(statisticsBuilder -> statisticsBuilder
+                                .setDiscontinuityTime(new DateAndTime(OffsetDateTime.now()
+                                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)))));
     }
 
     @Nonnull
-    public static TestInterface from(@Nonnull final NetworkInterface adapter) {
-        return new TestInterface(adapter);
+    public static Factory withIndex(@Nonnegative final int index) {
+        return new Factory(index);
     }
 
-    @Nonnull
-    public static TestInterface withName(@Nonnull final String name) {
-        try {
-            return new TestInterface(NetworkInterface.getByName(name));
+    public static class Factory {
 
-        } catch (SocketException e) {
-            throw new IllegalArgumentException(e);
+        @Nonnegative
+        private final int index;
+
+        private Factory(@Nonnegative final int index) {
+            this.index = index;
         }
-    }
 
-    @Nonnull
-    public static TestInterface fromConfigurationData(@Nonnull final TestInterface_Yang.Data data) {
-        @Nonnull final var instance = TestInterface.withName(data.getName().orElseThrow(() -> new IllegalArgumentException(
-                "No 'name' leaf value present in YANG Data")));
+        @Nonnull
+        public TestInterface fromAdapter(@Nonnull final NetworkInterface adapter) {
+            return new TestInterface(this.index, adapter);
+        }
 
-        instance.yangBinding.applyConfigurationData(data);
-        return instance;
+        @Nonnull
+        public TestInterface withName(@Nonnull final String name) {
+            try {
+                return new TestInterface(this.index, NetworkInterface.getByName(name));
+
+            } catch (SocketException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+
+        @Nonnull
+        public TestInterface fromConfigurationData(@Nonnull final TestInterface_Yang.Data data) {
+            @Nonnull final var instance = this.withName(data.requireName());
+            instance.yangBinding.applyConfigurationData(data);
+            return instance;
+        }
     }
 
     public void enable() {
