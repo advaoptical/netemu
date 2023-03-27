3f6e777822bf6ee99883882e946b2e040b6adfb1
Stefan Zimmermann <szimmermann@advaoptical.com>
2023-03-27 13:09:44 +0200

    testemu: Improve main() ++ Add RestconfService

--- testemu/src/main/java/com/adva/netemu/testemu/Main.java
+++ testemu/src/main/java/com/adva/netemu/testemu/Main.java
@@ -3,17 +3,19 @@ package com.adva.netemu.testemu;
 import javax.annotation.Nonnull;
 
 import com.adva.netemu.NetEmu;
-import com.adva.netemu.YangPool;
 import com.adva.netemu.northbound.NetconfService;
-import com.adva.netemu.northbound.PythonKernelService;
-import com.adva.netemu.northbound.WebService;
+// import com.adva.netemu.northbound.PythonKernelService;
+import com.adva.netemu.northbound.RestconfService;
+// import com.adva.netemu.northbound.WebService;
 
 
 public final class Main {
 
     public static void main(@Nonnull final String[] args) {
-        @Nonnull final var pool = new YangPool("testemu", NetEmuDefined.YANG_MODULE_INFOS);
-        @Nonnull final var device = pool.registerYangBindable(new TestDevice());
+        @Nonnull final var testEmu = NetEmu.withId("testemu").fromYangModuleInfos(NetEmuDefined.YANG_MODULE_INFOS);
+        @Nonnull final var testYangPool = testEmu.getYangPool();
+
+        @Nonnull final var device = testYangPool.registerYangBindable(new TestDevice());
         // final var intf = device.getInterfaces().get(0);
         // intf._buildIid();
 
@@ -28,15 +30,19 @@ public final class Main {
         System.out.println(intf.getIidBuilder().build());
         */
 
-        @Nonnull final var emu = NetEmu.forYangPool(pool);
-        emu.registerService(NetconfService.class);
-        emu.registerService(PythonKernelService.class);
-        emu.registerService(WebService.class);
+        testEmu.registerService(NetconfService.class)
+                .setStartingPort(17830);
+
+        testEmu.registerService(RestconfService.class)
+                .setPort(18080);
+
+        // emu.registerService(PythonKernelService.class);
+        // emu.registerService(WebService.class);
 
         // emu.loadConfigurationFromXml();
 
         // pool.writeOperationalDataFrom(device);
 
-        emu.start();
+        testEmu.start();
     }
 }
